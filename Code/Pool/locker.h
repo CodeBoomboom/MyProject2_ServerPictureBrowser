fb8cf1876b113908bb7b4f2f4fc67f7bbd5b7388
CodeBoomboom <xiaodexin0701@163.com>
2022-04-28 22:22:18 +0800

    add locker.h

---
+++ Code/Pool/locker.h
@@ -0,0 +1,54 @@
+/********************************************************************
+@FileName:locker.h
+@Version: 1.0
+@Notes:   线程同步机制封装类（互斥锁，条件变量）
+@Author:  XiaoDexin
+@Email:   xiaodexin0701@163.com
+@Date:    2022/04/28 22:06:32
+********************************************************************/
+#ifndef _LOCKER_H_
+#define _LOCKER_H_
+#include<pthread.h>
+#include<exception> //异常相关头文件
+
+//线程同步机制封装类
+
+//互斥锁类
+class locker {
+public:
+    locker(){
+        if(pthread_mutex_init(&m_mutex, NULL) != 0){
+            throw std::exception();
+        }
+    }
+
+    ~locker(){
+        pthread_mutex_destroy(&m_mutex);
+    }
+
+    bool lock(){
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+
+    bool unlock(){
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+
+    pehread_mutex_t * get(){
+        return &m_mutex;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+};
+
+
+
+
+
+
+
+
+
+
+#endif
