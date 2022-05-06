dfbe6d494c09d2a20548d5f50ec3e2cb42def073
CodeBoomboom <xiaodexin0701@163.com>
2022-05-06 17:31:53 +0800

    add 状态机

--- Code/Http/http_conn.h
+++ Code/Http/http_conn.h
@@ -38,6 +38,57 @@ public:
     static const int READ_BUFFER_SIZE = 2048;   //读缓冲大小
     static const int WRITE_BUFFER_SIZE = 1024;  //写缓冲大小
 
+    //HTTP请求方法，但我们只支持GET
+    enum METHOD{
+        GET = 0,
+        POST,
+        HEAD,
+        PUT,
+        DELETE,
+        TRACE,
+        OPTIONS,
+        CONNECT
+    };
+
+    /*解析客户端请求时，主状态机的状态
+    CHECK_STATE_REQUESTLINE:当前正在分析请求行
+    CHECK_STATE_HEADER:当前正在分析请求头
+    CHECK_STATE_CONTENT:当前正在解析请求体*/
+    enum CHECK_STATE{
+        CHECK_STATE_REQUESTLINE = 0,
+        CHECK_STATE_HEADER,
+        CHECK_STATE_CONTENT
+    };
+
+    /*服务器处理HTTP请求的可能结果，报文解析的结果
+        NO_REQUEST          :   请求不完整，需要继续读取客户数据
+        GET_REQUEST         :   表示获得了一个完整的客户请求
+        BAD_REQUEST         :   表示客户请求语法错误
+        NO_RESCOURCE        :   表示服务器没有资源
+        FORBIDDEN_REQUEST   :   表示客户对资源没有足够的访问权限
+        FILE_REQUEST        :   文件请求，获取文件成功
+        INTERNAL_ERROR      :   表示服务器内部错误
+        CLOSED_CONNECTION   :   表示客户端已关闭连接
+    */
+    enum HTTP_CODE{
+        NO_REQUEST,
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESCOURCE,
+        FORBIDDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION
+    };
+    
+    //从状态机的三种可能状态，即行的读取状态，分别表示
+    //1.读取到一个完整的行  2.行出错    3. 行数据尚且不完整
+    enum LINE_STATUS{
+        LINE_OK = 0,
+        LINE_BAD,
+        LINE_OPEN
+    };
+
     http_conn(){}
     ~http_conn(){}
 
